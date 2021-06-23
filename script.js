8a35d4f54c6a07fd1797abc24d2f9e751fd5064e
Tychebt <masse.ju35@gmail.com>
2021-06-23 11:59:44 +0200

    Ajout livre

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let myLibrary = [
+    {
+        title: "A Game of Thrones",
+        author: "George R. R. Martin",
+        pages: 694,
+        read: false
+      }
+];
+
 function Book (title, author, pages, read){
     this.title = title;
     this.author = author;
@@ -8,4 +17,9 @@ function Book (title, author, pages, read){
        console.log(res)
     }
 }
-const theHobbit = new Book('The hobbit', "JRR Tolkien", 295, "not read yet") 
+/* const theHobbit = new Book('The hobbit', "JRR Tolkien", 295, "not read yet") */
+
+function addBookToLibrary() {
+
+    let newBook = new Book(title, author, pages, read)
+  }
